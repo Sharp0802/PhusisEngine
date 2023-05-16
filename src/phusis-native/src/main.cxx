17719caf8797eb90d8c21da22e67cf885787f56f
Sharp0802 <sharp0802.business@gmail.com>
2023-05-16 10:56:53 +0900

    Add test code
    - src/phusis-native/src/main.cxx: Add test code

---
+++ src/phusis-native/src/main.cxx
@@ -0,0 +1,48 @@
+#include "fw.hxx"
+#include "phusis/application.hxx"
+#include "sys/logger.hxx"
+#include "sys/os.hxx"
+
+void vk_main();
+
+int32_t main() noexcept
+{
+	try
+	{
+		vk_main();
+		return 0;
+	}
+	catch (std::exception& exception)
+	{
+		std::cerr << "====== EXCEPTION ======" << '\n'
+				  << exception.what() << '\n'
+				  << "=======================" << '\n';
+		return -1;
+	}
+}
+
+void vk_main()
+{
+	const std::vector<std::string> layers = { "VK_LAYER_KHRONOS_validation" };
+	const std::vector<std::string> exts = {
+			"VK_KHR_surface",
+			"VK_EXT_swapchain_colorspace",
+			sys::os::surface
+	};
+
+	Phusis::Application app(layers, exts, Phusis::ApplicationMode::Quality);
+	if (!app.VkValidateLayer())
+		return;
+	if (!app.VkValidateExtension())
+		return;
+	if (!app.VkInitializeInstance())
+		return;
+	if (!app.VkInitializePhysicalDevice())
+		return;
+	if (!app.VkInitializeDeviceQueue())
+		return;
+
+	app.~Application();
+
+	sys::log.head(sys::INFO) << "complete operation successfully" << sys::EOM;
+}
