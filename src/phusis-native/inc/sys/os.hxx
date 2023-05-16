737cd997f7cb913166ec9650c3f9246b81af4e29
Sharp0802 <sharp0802.business@gmail.com>
2023-05-16 10:54:25 +0900

    Declare OS abstraction
    - src/phusis-native/inc/sys/os.hxx: Declare OS abstraction
    - src/phusis-native/src/sys/os.cxx: Define OS abstraction - EMPTY

---
+++ src/phusis-native/inc/sys/os.hxx
@@ -0,0 +1,46 @@
+#ifndef PHUSIS_OS_HXX
+#define PHUSIS_OS_HXX
+
+#include "fw.hxx"
+
+namespace sys
+{
+	using oskind = std::uint8_t;
+
+	class os
+	{
+	public:
+		enum : oskind
+		{
+			LINUX,
+			OSX,
+			WIN,
+			OTHER,
+			__N_ITEMS
+		};
+
+	public:
+		static const oskind kind =
+#if __linux__
+				LINUX
+#elif defined(__APPLE__) || defined(__MACH__)
+				OSX
+#elif defined(_WIN32) || defined(_WIN64)
+				WIN
+#else
+				OTHER
+#endif
+		;
+
+		static constexpr const char* surfaces[__N_ITEMS] = {
+				"VK_KHR_wayland_surface",
+				"VK_MVK_macos_surface",
+				"VK_KHR_win32_surface",
+				nullptr
+		};
+
+		static constexpr const char* surface = surfaces[kind];
+	};
+}
+
+#endif //PHUSIS_OS_HXX
