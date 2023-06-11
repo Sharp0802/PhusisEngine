f30a8083c50107d330ee1916d904d6f60b9641fc
Sharp0802 <sharp0802.business@gmail.com>
2023-06-11 22:27:32 +0900

    Refactor class
    - src/phusis-native/src/phusis/commandbuffer.cxx: Make class be able to be reused

--- src/phusis-native/src/phusis/commandbuffer.cxx
+++ src/phusis-native/src/phusis/commandbuffer.cxx
@@ -1,10 +1,17 @@
-#include "phusis/commandbuffer.hxx"
+#include "phusis/internal/commandbuffer.hxx"
 #include "sys/logger.hxx"
-#include "phusis/constantblock.hxx"
+#include "phusis/internal/constantblock.hxx"
 
-bool Phusis::CommandBuffer::Update() noexcept
+void Phusis::Internal::CommandBuffer::Load(EngineObjectData* data) noexcept
 {
-	if (!_data.enabled)
+	_data = data;
+}
+
+bool Phusis::Internal::CommandBuffer::Update() noexcept
+{
+	if (_data == nullptr)
+		return true;
+	if (!_data->enabled)
 		return true;
 
 	VkResult result;
@@ -22,8 +29,8 @@ bool Phusis::CommandBuffer::Update() noexcept
 	}
 
 	VkViewport viewport{};
-	viewport.width = static_cast<decltype(viewport.width)>(_width);
-	viewport.height = static_cast<decltype(viewport.height)>(_height);
+	viewport.width = static_cast<decltype(viewport.width)>(*_width);
+	viewport.height = static_cast<decltype(viewport.height)>(*_height);
 	viewport.maxDepth = 1.f;
 	viewport.minDepth = 0.f;
 	viewport.x = 0.f;
@@ -32,29 +39,29 @@ bool Phusis::CommandBuffer::Update() noexcept
 	VkRect2D scissor{};
 	scissor.offset.x = 0;
 	scissor.offset.y = 0;
-	scissor.extent.width = _width;
-	scissor.extent.height = _height;
+	scissor.extent.width = *_width;
+	scissor.extent.height = *_height;
 
 	vkCmdSetViewport(_buffer, 0, 1, &viewport);
 	vkCmdSetScissor(_buffer, 0, 1, &scissor);
 	vkCmdBindPipeline(_buffer, VK_PIPELINE_BIND_POINT_GRAPHICS, _pipeline);
 
-	ConstantBlock blk(_projection * _view * _data.Rotation, _data.Color);
+	ConstantBlock blk(_projection * _view * _data->Rotation, _data->Color);
 	vkCmdPushConstants(_buffer, _layout, VK_SHADER_STAGE_VERTEX_BIT, 0, sizeof(ConstantBlock), &blk);
 
-	std::vector<VkDeviceSize> offsets(_data.Mesh.Vertices.size());
+	std::vector<VkDeviceSize> offsets(_data->Mesh.Vertices.size());
 	offsets.assign(offsets.size(), 0);
 
-	std::vector<VkBuffer> buffers(_data.Mesh.Vertices.size());
+	std::vector<VkBuffer> buffers(_data->Mesh.Vertices.size());
 	for (size_t i = 0; i < buffers.size(); ++i)
-		buffers[i] = _data.Mesh.Vertices[i].Array;
+		buffers[i] = _data->Mesh.Vertices[i].Array;
 
 	vkCmdBindVertexBuffers(_buffer, 0, buffers.size(), buffers.data(), offsets.data());
 
 	for (size_t i = 0; i < buffers.size(); ++i)
 	{
 		vkCmdBindIndexBuffer(_buffer, buffers[i], 0, VK_INDEX_TYPE_UINT32);
-		vkCmdDrawIndexed(_buffer, _data.Mesh.Indices[i].Count, 1, 0, 0, 0);
+		vkCmdDrawIndexed(_buffer, _data->Mesh.Indices[i].Count, 1, 0, 0, 0);
 	}
 
 	result = vkEndCommandBuffer(_buffer);
